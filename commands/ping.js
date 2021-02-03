92ad16855488e29214db23fcef254357a55778b3
Neikinikless <67499407+Neikinikless@users.noreply.github.com>
2021-02-03 07:48:41 +0100

    Removed Ping.js (Issues) :x:
    Early Removed Ping.js
    
    Soon Updated Version

--- commands/ping.js
+++ commands/ping.js
@@ -1,32 +1,4 @@
 
 //version: Title
 
-const { Client } = require("discord.js");
-const client = new Client
-
-module.exports = {
-    name: "ping",
-    aliases: ["latency"],
-    description: "The Latency of the Bot (PING)",
-   async execute(message) {
-
-        const Discord = require('discord.js');
-        //const client = new Discord.Client();
-        const msg = await message.channel.send("Pinging...");
-        const Embed = new Discord.MessageEmbed()
-          .setTitle("Bot Latency.")
-          .setAuthor(`${message.author.username}` , message.author.displayAvatarURL())
-          .setDescription(
-            `⌛ Latency is ${Math.floor(
-              msg.createdTimestamp - message.createdTimestamp
-            )}ms\n⏲️ API Ping is ${Math.round(message.client.ws.ping)}ms`
-          )
-          .setColor('RANDOM');
-        msg.edit(Embed);
-        msg.edit("\u200b");
-          
-  
-
-    }
-
-  };
+//ISSUES
