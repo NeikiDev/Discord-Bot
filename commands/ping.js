49b2cff8097b79c70ef21e40b6bf02844f975854
Neikinikless <67499407+Neikinikless@users.noreply.github.com>
2021-02-03 07:53:44 +0100

    Added ping.js (1.0.7) :white_check_mark:
    Added Updatet Version of Ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,33 @@
 
 //version: Title
 
-//ISSUES
+
+const { Client } = require("discord.js");
+const client = new Client
+
+module.exports = {
+    name: "ping",
+    aliases: ["latency"],
+    description: "The Latency of the Bot",
+   async execute(message) {
+
+        const Discord = require('discord.js');
+        const msg = await message.channel.send("Pinging Bot & API....");
+        const Embed = new Discord.MessageEmbed()
+          .setTitle("Bot Latency.")
+          .setAuthor(`${message.author.username}` , message.author.displayAvatarURL())
+          .setDescription(
+            `⌛ Latency is ${Math.floor(
+              msg.createdTimestamp - message.createdTimestamp
+            )}ms\n🌐 API Ping is ${Math.round(message.client.ws.ping)}ms`
+          )
+          .setColor('GREEN')
+          .setFooter(`https://github.com/NeikiDev/Discord-Bot`)
+        msg.edit(Embed);
+        msg.edit("\u200b");
+          
+  
+
+    }
+
+  };
